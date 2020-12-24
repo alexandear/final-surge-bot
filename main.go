342588adae75e1fffdde07a5487072bbc83aba5e
Oleksandr Redko <oleksandr.red+github@gmail.com>
2020-12-24 21:29:34 +0200

    Save user tokens to database

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -12,6 +13,13 @@ import (
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	bolt "go.etcd.io/bbolt"
+)
+
+const (
+	DatabaseFile = "final-surge-bot.db"
+
+	BucketUserToken = "UserToken"
 )
 
 const (
@@ -22,7 +30,8 @@ const (
 type EnterCred int
 
 const (
-	EnterLogin EnterCred = iota + 1
+	EnterUnknown EnterCred = iota
+	EnterLogin
 	EnterPassword
 	EnterDone
 )
@@ -58,12 +67,37 @@ type FinalSurgeWorkoutList struct {
 	}
 }
 
+type UserToken struct {
+	UserKey string
+	Token   string
+}
+
 func main() {
 	apiKey := os.Getenv("BOT_API_KEY")
 
 	bot, err := tgbotapi.NewBotAPI(apiKey)
 	if err != nil {
-		log.Panic(err)
+		log.Panic(fmt.Errorf("failed to init bot api: %w", err))
+	}
+
+	db, err := bolt.Open(DatabaseFile, 0o600, &bolt.Options{Timeout: time.Second})
+	if err != nil {
+		log.Fatal(fmt.Errorf("failed to open database: %w", err))
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Println(fmt.Errorf("failed to close db: %w", err))
+		}
+	}()
+
+	if err := db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucket([]byte(BucketUserToken))
+		if errors.Is(err, bolt.ErrBucketExists) {
+			return nil
+		}
+		return err
+	}); err != nil {
+		log.Fatal(fmt.Errorf("failed to create bucket: %w", err))
 	}
 
 	log.Printf("authorized on account %s", bot.Self.UserName)
@@ -81,54 +115,49 @@ func main() {
 			continue
 		}
 
+		userName := update.Message.From.UserName
+
 		if update.Message.IsCommand() {
 			switch update.Message.Command() {
 			case CommandStart:
 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Enter FinalSurge email:")
 				if _, err := bot.Send(msg); err != nil {
-					log.Printf("error: %v\n", err)
+					log.Println(fmt.Errorf("failed to send msg about email: %w", err))
 					continue
 				}
 
-				userEnterCreds[update.Message.From.UserName] = EnterLogin
-				userCreds[update.Message.From.UserName] = &FinalSurgeCred{}
+				userEnterCreds[userName] = EnterLogin
+				userCreds[userName] = &FinalSurgeCred{}
 			case CommandTask:
-				client := http.Client{Timeout: 2 * time.Second}
-				cred := userCreds[update.Message.From.UserName]
-				b, err := json.Marshal(&cred)
-				if err != nil {
-					log.Fatal(err)
-				}
-				resp, err := client.Post("https://beta.finalsurge.com/api/Data?request=login", "",
-					bytes.NewReader(b))
-				if err != nil {
-					log.Fatal(err)
+				var userToken UserToken
+				if err := db.View(func(tx *bolt.Tx) error {
+					b := tx.Bucket([]byte(BucketUserToken))
+					bc := b.Get([]byte(userName))
+					if bc == nil {
+						log.Println("token not found for user: ", userName)
+						return nil
+					}
+
+					if err := json.Unmarshal(bc, &userToken); err != nil {
+						return fmt.Errorf("failed to unmarshal usertoken: %w", err)
+					}
+					return nil
+				}); err != nil {
+					log.Fatal(fmt.Errorf("failed to get user token: %w", err))
 				}
 
-				bs, err := ioutil.ReadAll(resp.Body)
-				if err != nil {
-					log.Fatal(err)
-				}
-				resp.Body.Close()
-
-				var login FinalSurgeLogin
-				if err := json.Unmarshal(bs, &login); err != nil {
-					log.Fatal(err)
-				}
-
-				if !login.Success && login.ErrorNumber != nil && login.ErrorDescription != nil {
-					log.Println("error: ", *login.ErrorNumber, *login.ErrorDescription)
+				if userToken.UserKey == "" {
 					continue
 				}
 
-				log.Println("logged as " + login.Data.FirstName + " " + login.Data.LastName)
+				log.Println("logged as " + userToken.UserKey)
 
 				u, err := url.Parse("https://beta.finalsurge.com/api/Data?request=WorkoutList&scope=USER")
 				if err != nil {
-					log.Fatal()
+					log.Panic(fmt.Errorf("failed to parse url: %w", err))
 				}
 				q := u.Query()
-				q.Set("scopekey", login.Data.UserKey)
+				q.Set("scopekey", userToken.UserKey)
 				today := time.Now().Format("2006-01-02")
 				q.Set("startdate", today)
 				q.Set("enddate", today)
@@ -136,20 +165,23 @@ func main() {
 
 				req, err := http.NewRequest(http.MethodGet, u.String(), nil)
 				if err != nil {
-					log.Fatal(err)
+					log.Fatal(fmt.Errorf("failed to create request: %w", err))
 				}
-				req.Header.Set("Authorization", "Bearer "+login.Data.Token)
+				req.Header.Set("Authorization", "Bearer "+userToken.Token)
 
-				resp, err = client.Do(req)
+				client := http.Client{Timeout: 2 * time.Second}
+				resp, err := client.Do(req)
 				if err != nil {
-					log.Fatal(err)
+					log.Fatal(fmt.Errorf("failed to do request: %w", err))
 				}
 
-				bs, err = ioutil.ReadAll(resp.Body)
+				bs, err := ioutil.ReadAll(resp.Body)
 				if err != nil {
-					log.Fatal(err)
+					log.Fatal(fmt.Errorf("failed to read all: %w", err))
 				}
-				resp.Body.Close()
+				if err := resp.Body.Close(); err != nil {
+					log.Fatal(fmt.Errorf("failed to close body: %w", err))
+				}
 
 				log.Println("raw workouts resp: " + string(bs))
 
@@ -159,7 +191,7 @@ func main() {
 				}
 
 				if !workoutList.Success && workoutList.ErrorNumber != nil && workoutList.ErrorDescription != nil {
-					log.Println("error: ", *workoutList.ErrorNumber, *workoutList.ErrorDescription)
+					log.Println(fmt.Errorf("failed to get workout list: %d %s", *workoutList.ErrorNumber, *workoutList.ErrorDescription))
 					continue
 				}
 
@@ -174,33 +206,71 @@ func main() {
 			continue
 		}
 
-		switch enter := userEnterCreds[update.Message.From.UserName]; enter {
+		switch enter := userEnterCreds[userName]; enter {
 		case EnterLogin:
-			cred := userCreds[update.Message.From.UserName]
+			cred := userCreds[userName]
 			cred.Email = update.Message.Text
 
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Enter FinalSurge password:")
 			if _, err := bot.Send(msg); err != nil {
-				log.Printf("error: %v\n", err)
+				log.Println(fmt.Errorf("failed to send msg about password: %w", err))
 				continue
 			}
 
-			userEnterCreds[update.Message.From.UserName] = EnterPassword
+			userEnterCreds[userName] = EnterPassword
 		case EnterPassword:
-			cred := userCreds[update.Message.From.UserName]
+			cred := userCreds[userName]
 			cred.Password = update.Message.Text
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Got it: %v", userCreds[update.Message.From.UserName]))
-			if _, err := bot.Send(msg); err != nil {
-				log.Printf("failed to send: %v\n", err)
-				continue
+			userEnterCreds[userName] = EnterDone
+			fallthrough
+		case EnterUnknown, EnterDone:
+			cred := userCreds[userName]
+
+			b, err := json.Marshal(&cred)
+			if err != nil {
+				log.Fatal(fmt.Errorf("failed to marshal cred: %w", err))
 			}
-			userEnterCreds[update.Message.From.UserName] = EnterDone
-		case EnterDone:
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Cred exists: %v", userCreds[update.Message.From.UserName]))
-			if _, err := bot.Send(msg); err != nil {
-				log.Printf("unknown error: %v\n", err)
-				continue
+
+			client := http.Client{Timeout: 2 * time.Second}
+			resp, err := client.Post("https://beta.finalsurge.com/api/Data?request=login", "",
+				bytes.NewReader(b))
+			if err != nil {
+				log.Fatal(fmt.Errorf("failed to do post request: %w", err))
+			}
+
+			bs, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatal(fmt.Errorf("failed to read all: %w", err))
+			}
+			if err := resp.Body.Close(); err != nil {
+				log.Fatal(fmt.Errorf("failed to close body: %w", err))
+			}
+
+			var login FinalSurgeLogin
+			if err := json.Unmarshal(bs, &login); err != nil {
+				log.Fatal(fmt.Errorf("failed to unmarshal login: %w", err))
+			}
+
+			if !login.Success && login.ErrorNumber != nil && login.ErrorDescription != nil {
+				log.Fatal(fmt.Errorf("failed to get login data: %d %s", *login.ErrorNumber, *login.ErrorDescription))
+			}
+
+			userToken := &UserToken{
+				UserKey: login.Data.UserKey,
+				Token:   login.Data.Token,
+			}
+
+			bu, err := json.Marshal(userToken)
+			if err != nil {
+				log.Fatal(fmt.Errorf("failed to marshal user token: %w", err))
+			}
+
+			if err := db.Update(func(tx *bolt.Tx) error {
+				b := tx.Bucket([]byte(BucketUserToken))
+				return b.Put([]byte(userName), bu)
+			}); err != nil {
+				log.Fatal(fmt.Errorf("failed to put user token: %w", err))
 			}
 		default:
 			log.Fatal(fmt.Sprintf("unknown enter value %d", enter))
